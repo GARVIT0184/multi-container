3d1abb437098d12c6e7697a9c38938daa4fd54b9
24f2006741 <24f2006741@ds.study.iitm.ac.in>
2026-07-06 13:27:20 +0530

    main.py

---
+++ app/main.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI
+import redis
+
+app = FastAPI()
+
+r = redis.Redis(
+    host="redis",
+    port=6379,
+    decode_responses=True
+)
+
+
+@app.get("/")
+def root():
+    return {"message": "API running"}
+
+
+@app.post("/hit/{key}")
+def hit(key: str):
+    count = r.incr(key)
+    return {
+        "key": key,
+        "count": count
+    }
+
+
+@app.get("/count/{key}")
+def count(key: str):
+    value = r.get(key)
+
+    if value is None:
+        value = 0
+
+    return {
+        "key": key,
+        "count": int(value)
+    }
+
+
+@app.get("/healthz")
+def health():
+    try:
+        r.ping()
+        return {
+            "status": "ok",
+            "redis": "up"
+        }
+    except:
+        return {
+            "status": "error",
+            "redis": "down"
+        }
